462c3b4a4a4cb2ceea6c84e0c6e81f8a9f6c49f0
Justin Ly <jly3@ualberta.ca>
2021-02-10 01:20:49 +0000

    added setupInput and handleUserInput

--- play.js
+++ play.js
@@ -1,3 +1,29 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+const setupInput = function() {
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+
+  stdin.setEncoding('utf8');
+
+  stdin.resume();
+
+  const handleUserInput = stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    
+  })
+
+  return stdin;
+}
+
+setupInput();
